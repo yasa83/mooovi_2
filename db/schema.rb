336bce57c5612a6a7b6d9ad019bc2ea658a184b8
Your Name <you@example.com>
2018-12-16 22:49:18 +0900

    [Add]-scraping-data
    2章最後まで

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_16_115039) do
+ActiveRecord::Schema.define(version: 2018_12_16_131341) do
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "director"
+    t.text "detail"
+    t.string "open_date"
   end
 
 end
